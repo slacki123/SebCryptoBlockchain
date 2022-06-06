5c4a0f0a8e0895a1db1c0cbb236129ae87a887ad
Sebastian <sebastian.lacki1@gmail.com>
2022-06-06 21:23:43 +0100

    fix starting bugs

--- backend/util/try_retrieve_local_blockchain.py
+++ backend/util/try_retrieve_local_blockchain.py
@@ -4,14 +4,18 @@ import os
 from backend.blockchain.blockchain import Blockchain
 
 
-def try_retrieve_local_blockchain(file_path) -> Blockchain or None:
+def try_retrieve_local_blockchain(file_path) -> Blockchain:
     if not file_path:
-        return None
+        return Blockchain()
 
     if os.path.exists(file_path):
         with open(file_path, 'r') as f:
             result_text = f.read()
             resulting_blockchain = json.loads(result_text) if result_text else None
             return Blockchain.from_json(resulting_blockchain)
+
+    return Blockchain()
+
+
 
 
